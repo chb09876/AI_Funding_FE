16a783c588377ccd3648e6e0575c0f839f7f46d5
chb09876 <chb09876@naver.com>
2022-01-11 12:49:23 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,30 @@
+import styled from "styled-components";
+import Home from "./pages/home/Home";
+import GlobalStyle from "./style/GlobalStyle";
+
 function App() {
-  return <div className="App">Hello, ToBi!</div>;
+  return (
+    <Back className="App">
+      <GlobalStyle />
+      <Home></Home>
+      <Nav></Nav>
+    </Back>
+  );
 }
 
 export default App;
+
+const Back = styled.div`
+  background-color: #616060;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  width: 100%;
+  height: 100%;
+`;
+
+const Nav = styled.div`
+  height: 70px;
+  width: 100%;
+  border: solid 1px blue;
+`;
