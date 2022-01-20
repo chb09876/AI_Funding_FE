cd4f3e9c1b3deb1b9fccadff62ee3fd63b69e7da
angrymusic <58168512+angrymusic@users.noreply.github.com>
2022-01-20 20:09:51 +0900

    AI page 2차 구현
    1. 현재 종목탭 구현
    (종목이 많아졌을 때 스크롤 미구현)

--- src/pages/AI-pages/presentation/tabs/CurrentStuck.jsx
+++ src/pages/AI-pages/presentation/tabs/CurrentStuck.jsx
@@ -1,3 +1,29 @@
+import StuckInfo from "../components/StuckInfo";
+
 export default function CurrentStuck() {
-  return <div>현재 종목</div>;
+  return (
+    <div className="container">
+      <StuckInfo
+        StuckName="카카오"
+        CurrentPrice="112,500"
+        PriceFluc="1,500"
+        RateFluc="-1.32%"
+        Fluc="down"
+      />
+      <StuckInfo
+        StuckName="삼성전자"
+        CurrentPrice="80,200"
+        PriceFluc="7,000"
+        RateFluc="+9.28%"
+        Fluc="up"
+      />
+      <StuckInfo
+        StuckName="광운대"
+        CurrentPrice="40,200"
+        PriceFluc="0"
+        RateFluc="=0.00%"
+        Fluc="none"
+      />
+    </div>
+  );
 }
