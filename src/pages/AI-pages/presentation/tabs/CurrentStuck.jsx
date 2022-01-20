21ce8500a723a453f195eb6374141386c2d7ffe7
angrymusic <58168512+angrymusic@users.noreply.github.com>
2022-01-20 20:26:44 +0900

    2차 추가 수정
    1. 스크롤 구현

--- src/pages/AI-pages/presentation/tabs/CurrentStuck.jsx
+++ src/pages/AI-pages/presentation/tabs/CurrentStuck.jsx
@@ -1,8 +1,8 @@
 import StuckInfo from "../components/StuckInfo";
-
+import styled from "styled-components";
 export default function CurrentStuck() {
   return (
-    <div className="container">
+    <StyledScrollArea className="container">
       <StuckInfo
         StuckName="카카오"
         CurrentPrice="112,500"
@@ -24,6 +24,53 @@ export default function CurrentStuck() {
         RateFluc="=0.00%"
         Fluc="none"
       />
-    </div>
+      <StuckInfo
+        StuckName="카카오"
+        CurrentPrice="112,500"
+        PriceFluc="1,500"
+        RateFluc="-1.32%"
+        Fluc="down"
+      />
+      <StuckInfo
+        StuckName="삼성전자"
+        CurrentPrice="80,200"
+        PriceFluc="7,000"
+        RateFluc="+9.28%"
+        Fluc="up"
+      />
+      <StuckInfo
+        StuckName="광운대"
+        CurrentPrice="40,200"
+        PriceFluc="0"
+        RateFluc="=0.00%"
+        Fluc="none"
+      />
+      <StuckInfo
+        StuckName="카카오"
+        CurrentPrice="112,500"
+        PriceFluc="1,500"
+        RateFluc="-1.32%"
+        Fluc="down"
+      />
+      <StuckInfo
+        StuckName="삼성전자"
+        CurrentPrice="80,200"
+        PriceFluc="7,000"
+        RateFluc="+9.28%"
+        Fluc="up"
+      />
+      <StuckInfo
+        StuckName="광운대"
+        CurrentPrice="40,200"
+        PriceFluc="0"
+        RateFluc="=0.00%"
+        Fluc="none"
+      />
+    </StyledScrollArea>
   );
 }
+
+const StyledScrollArea = styled.div`
+  overflow: scroll;
+  height: 85vh;
+`;
