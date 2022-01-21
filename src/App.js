13d724244323d96ef432ef942fb3eeecd3e0feae
chb09876 <chb09876@naver.com>
2022-01-21 14:15:37 +0900

    #21 Route /login 페이지 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Route, Routes } from "react-router-dom";
-import BackgroundLayout from "./sample/common/BackgroundLayout";
-import MenuNav from "./sample/common/MenuNav";
+import BackgroundLayout from "./common/BackgroundLayout";
+import MenuNav from "./common/MenuNav";
 import Home from "./sample/pages/home/presentational/Home";
+import Login from "./pages/login/presentational/Login";
 // import BackgroundLayout from "./common/BackgroundLayout";
 
 export default function App() {
   return (
     <BackgroundLayout className="App">
       <Routes>
+        <Route path="/login" element={<Login></Login>} />
         <Route path="/" element={<Home></Home>} />
         <Route path="/second" element={"AI page"} />
         <Route path="/third" element={"Account page"} />
