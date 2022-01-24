4b7a606cf6e13d5fe87400f29db5f674624111bc
angrymusic <58168512+angrymusic@users.noreply.github.com>
2022-01-24 17:07:33 +0900

    거래내역 구현
    detail 태그 사용

--- src/pages/AI-pages/presentation/tabs/TransHistory.jsx
+++ src/pages/AI-pages/presentation/tabs/TransHistory.jsx
@@ -1,3 +1,135 @@
+import styled, { css } from "styled-components";
+import { useState } from "react";
+
 export default function TransHistory() {
-  return <div>거래내역</div>;
+  const [SelectedTerm, SelectTerm] = useState(0);
+  return (
+    <StyledHistoryContainer className="HistoryContainer">
+      <StyledTermSelector className="SelectTerm">
+        <StyledTermButton
+          className={SelectedTerm === 0 ? "selected" : "notselected"}
+          onClick={() => SelectTerm(0)}
+        >
+          ALL
+        </StyledTermButton>
+        <StyledBar>|</StyledBar>
+        <StyledTermButton
+          className={SelectedTerm === 1 ? "selected" : "notselected"}
+          onClick={() => SelectTerm(1)}
+        >
+          1M
+        </StyledTermButton>
+        <StyledBar>|</StyledBar>
+        <StyledTermButton
+          className={SelectedTerm === 3 ? "selected" : "notselected"}
+          onClick={() => SelectTerm(3)}
+        >
+          3M
+        </StyledTermButton>
+        <StyledBar>|</StyledBar>
+        <StyledTermButton
+          className={SelectedTerm === 6 ? "selected" : "notselected"}
+          onClick={() => SelectTerm(6)}
+        >
+          6M
+        </StyledTermButton>
+      </StyledTermSelector>
+      <StyledHistories className="Histories">
+        <StyledDetails>
+          <summary>
+            <div style={{ fontSize: "1.4rem", fontWeight: 700 }}>종목 이름</div>
+            <div style={{ fontSize: "0.8rem", fontWeight: 300, color: "gray" }}>
+              거래 날짜
+            </div>
+          </summary>
+          <StyledDetailsContainer>
+            <StyledDetailsContent>
+              거래종류 : 주식 매도 / 주식 매수
+              <br />
+              거래수량 : 00
+              <br />
+              단가 : 000,000원
+              <br />
+              매수가 : 000,000원
+            </StyledDetailsContent>
+          </StyledDetailsContainer>
+        </StyledDetails>
+        <StyledDetails>
+          <summary>
+            <div style={{ fontSize: "1.4rem", fontWeight: 700 }}>종목 이름</div>
+            <div style={{ fontSize: "0.8rem", fontWeight: 300, color: "gray" }}>
+              거래 날짜
+            </div>
+          </summary>
+          <StyledDetailsContainer>
+            <StyledDetailsContent>
+              거래종류 : 주식 매도 / 주식 매수
+              <br />
+              거래수량 : 00
+              <br />
+              단가 : 000,000원
+              <br />
+              매수가 : 000,000원
+            </StyledDetailsContent>
+          </StyledDetailsContainer>
+        </StyledDetails>
+      </StyledHistories>
+    </StyledHistoryContainer>
+  );
 }
+
+const StyledHistoryContainer = styled.div`
+  margin: 10px;
+`;
+const StyledTermSelector = styled.div`
+  margin: auto;
+`;
+const StyledTermButton = styled.span`
+  font-weight: bold;
+  margin: 0 8px;
+  font-size: 1.1rem;
+  ${(props) => {
+    return props.className === "selected"
+      ? `color: rgb(184, 168, 142)`
+      : `color: rgb(119, 119, 119)`;
+  }};
+`;
+
+const StyledBar = styled.span`
+  font-weight: bold;
+  color: rgb(151, 151, 151);
+`;
+
+const StyledHistories = styled.div`
+  padding: 0 10px;
+`;
+
+const StyledDetails = styled.details`
+  & > summary {
+    color: white;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border-bottom: 2px solid rgb(184, 168, 142);
+    // 삼각형 제거
+    list-style: none;
+  }
+  & > summary::marker {
+    display: none;
+  }
+  &[open] > summary {
+    border-bottom: 2px dashed rgb(184, 168, 142);
+  }
+`;
+
+const StyledDetailsContent = styled.div`
+  color: white;
+  margin: auto;
+  text-align: center;
+  font-size: 16px;
+`;
+const StyledDetailsContainer = styled.div`
+  margin-top: 10px;
+  padding-bottom: 10px;
+  width: 100%;
+  border-bottom: 2px solid rgb(184, 168, 142);
+`;
