d9217c969d053eb787a9bf6515bfeabffe294926
chb09876 <chb09876@naver.com>
2022-02-05 23:05:00 +0900

    feat: App.js 로그인 페이지 먼저나오도록 수정
    로그인 페이지가 먼저나오도록 수정했습니다. 내부 state로 isLoggedIn을 가져 로그인 상태에 따라 렌더링하는 Route가 다르도록 구현하였습니다. 그리고 정의되지 않은 주소에 접근시 404가 뜨도록 하였습니다. 나중에 404페이지를 만들거나 Home으로 redirect되도록 하여도 괜찮을 것 같습니다.

--- src/App.js
+++ src/App.js
@@ -3,20 +3,49 @@ import BackgroundLayout from "./common/BackgroundLayout";
 import MenuNav from "./common/MenuNav";
 import Home from "./sample/pages/home/presentational/Home";
 import Login from "./pages/login/container/Login";
-// import BackgroundLayout from "./common/BackgroundLayout";
+import { useState } from "react";
 
 export default function App() {
+  // 해당 state는 임시로 만들어둔 것. 추후 수정 예정.
+  // isLoggedIn이 true면 로그인이 된 상태로 로그인 페이지 이외의 페이지에 접속할 수 있습니다.
+  // isLoggedIn이 false면 로그인페이지만 접속할 수 있고 나머지는 접속할 수 없습니다.
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   return (
     <BackgroundLayout className="App">
       <Routes>
-        <Route path="/login" element={<Login></Login>} />
-        <Route path="/" element={<Home></Home>} />
-        <Route path="/second" element={"AI page"} />
-        <Route path="/third" element={"Account page"} />
-        <Route path="/fourth" element={"Community page"} />
-        <Route path="/fifth" element={"Settings page"} />
+        {isLoggedIn ? (
+          <>
+            <Route path="/" element={<Home />} />
+            <Route path="/second" element={"AI page"} />
+            <Route path="/third" element={"Account page"} />
+            <Route path="/fourth" element={"Community page"} />
+            <Route path="/fifth" element={"Settings page"} />
+            <Route path="*" element={"404"} />
+          </>
+        ) : (
+          <>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Login />
+                  <button
+                    onClick={() => {
+                      // 개발용 로그인 스킵 버튼.
+                      // set isLoggedIn True
+                      setIsLoggedIn(true);
+                    }}
+                  >
+                    Skip
+                  </button>
+                </>
+              }
+            />
+            <Route path="*" element={"404"} />
+          </>
+        )}
       </Routes>
-      <MenuNav></MenuNav>
+      {isLoggedIn && <MenuNav />}
     </BackgroundLayout>
   );
 }
