d1cb29989de11496dd79476cd3af7a2dd659251f
김호성 <000731khs@naver.com>
2022-02-12 21:09:58 +0900

    리덕스 기반 작업 복구

--- src/index.js
+++ src/index.js
@@ -5,13 +5,20 @@ import reportWebVitals from "./reportWebVitals";
 import App from "./App";
 import GlobalStyle from "./style/GlobalStyle";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <GlobalStyle />
-      <App />
-    </BrowserRouter>
+    <GlobalStyle />
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
