b0ab660e1757b924c668666891a09966423238d1
syw2045 <syw2045@naver.com>
2022-02-15 23:17:51 +0900

    Account
    Account

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { Route, Routes } from 'react-router-dom';
-import BackgroundLayout from './sample/common/BackgroundLayout';
-import MenuNav from './sample/common/MenuNav';
-import Home from './sample/pages/home/presentational/Home';
-// import BackgroundLayout from "./common/BackgroundLayout";
+import { Route, Routes } from "react-router-dom";
+import BackgroundLayout from "./sample/common/BackgroundLayout";
+import MenuNav from "./sample/common/MenuNav";
+import Home from "./sample/pages/home/presentational/Home";
+import AccountPage from "./pages/Accountpage/presentational/AccountPage";
 
 export default function App() {
   return (
     <BackgroundLayout className="App">
       <Routes>
         <Route path="/" element={<Home></Home>} />
-        <Route path="/second" element={'AI page'} />
-        <Route path="/third" element={'Account page'} />
-        <Route path="/fourth" element={'Community page'} />
-        <Route path="/fifth" element={'Settings page'} />
+        <Route path="/second" element={"AI pages"} />
+        <Route path="/third" element={<AccountPage></AccountPage>}/>
+        <Route path="/fourth" element={"Community page"} />
+        <Route path="/fifth" element={"Settings page"} />
       </Routes>
       <MenuNav></MenuNav>
     </BackgroundLayout>
---
+++ src/pages/Accountpage/presentational/AccountPage.jsx
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import CheckProfit from './CheckProfit';
+import CompareProfit from './CompareProfit';
+
+
+const obj = {
+  0: <CheckProfit />,
+  1: <CompareProfit />,
+};
+
+export default function AccountPage() {
+  const [SelectedTab, SelectTab] = useState(0);
+  return (
+    <StyledAccountPage className="ai-page-container">
+      <StyledTabs className="tabs">
+        <StyledTab>
+          <StyledTabButton
+            className={SelectedTab === 0 ? 'selected' : 'notselected'}
+            onClick={() => SelectTab(0)}
+          >
+            수익 확인
+          </StyledTabButton>
+        </StyledTab>
+        <StyledTab>
+          <StyledTabButton
+            className={SelectedTab === 1 ? 'selected' : 'notselected'}
+            onClick={() => SelectTab(1)}
+          >
+            수익 비교
+          </StyledTabButton>
+        </StyledTab>
+      </StyledTabs>
+      <StyledContent className="contents">{obj[SelectedTab]}</StyledContent>
+    </StyledAccountPage>
+  );
+}
+
+const StyledAccountPage = styled.div`
+  height: 100%;
+`;
+const StyledTabs = styled.div`
+  display: flex;
+  width: 100%;
+  height: 60px;
+  background: black;
+`;
+
+const StyledTab = styled.div`
+  margin-top: 10px;
+  width: 50%;
+`;
+
+const StyledTabButton = styled.div`
+  width: 100%;
+  text-align: center;
+  height: 100%;
+  line-height: 45px;
+  font-size: 18px;
+  ${(props) => {
+    if (props.className === 'selected') {
+      return `color: rgb(152, 128, 101);
+      font-weight:bold;
+      
+      border-right: 2px solid rgb(152, 128, 101);
+      border-left: 2px solid rgb(152, 128, 101);
+      border-top: 2px solid rgb(152, 128, 101);
+      border-radius:15px 15px 0px 0px;
+      `;
+    } else if (props.className === 'notselected') {
+      return `color: rgb(119, 119, 119);
+      border-bottom: 2px solid rgb(152, 128, 101);
+      
+      `;
+    }  else {
+      return `color: rgb(119, 119, 119);
+      border-bottom: 2px solid rgb(152, 128, 101);
+      `;
+    }
+  }};
+`;
+
+const StyledContent = styled.div``;
