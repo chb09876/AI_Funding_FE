86a5e1dc5a24acc47ae6e5a1d69ece1971b5edc4
chb09876 <chb09876@naver.com>
2022-03-07 01:23:25 +0900

    chore: Kakao로그인 버튼 a태그로 교체

--- src/pages/login/presentational/KakaoLoginButton.jsx
+++ src/pages/login/presentational/KakaoLoginButton.jsx
@@ -1,21 +1,16 @@
 import styled from 'styled-components';
 import kakao_img from '../../../image/kakao_login_large_narrow.png';
 
-export default function KakaoLoginButton({ className, login }) {
+export default function KakaoLoginButton({ className }) {
   return (
     <KakaoButton
+      href="http://ec2-13-209-73-79.ap-northeast-2.compute.amazonaws.com:8080/auth/KAKAO"
       className={className}
-      onClick={
-        login ||
-        (() => {
-          alert('로그인 함수를 넣어주세요.');
-        })
-      }
-    ></KakaoButton>
+    />
   );
 }
 
-const KakaoButton = styled.button`
+const KakaoButton = styled.a`
   background-image: url(${kakao_img});
   background-size: contain;
 
