47794ad0c91d7f0b820badfaeb8a72feec057748
chb09876 <chb09876@naver.com>
2022-03-12 18:37:04 +0900

    chore: 계좌 수익률 차트 추가 및 자잘한 수정

--- src/pages/Accountpage/presentational/CompareProfit.jsx
+++ src/pages/Accountpage/presentational/CompareProfit.jsx
@@ -1,48 +1,49 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 import { useState } from 'react';
+import AccountLineChart from './AccountLineChart';
 
 export default function CompareProfit() {
-  const [SelectedTerm, SelectTerm] = useState(0);
+  const [selectedTerm, SelectTerm] = useState(0);
   return (
-      <StyledCompareProfit className = "compareProfit">
-        <StyledTop className = "term">
+    <StyledCompareProfit className="compareProfit">
+      <StyledTop className="term">
         <StyledTermButton
-          className={SelectedTerm === 0 ? 'selected' : 'notselected'}
+          className={selectedTerm === 0 ? 'selected' : 'notselected'}
           onClick={() => SelectTerm(0)}
         >
           ALL
         </StyledTermButton>
         <StyledBar>|</StyledBar>
         <StyledTermButton
-          className={SelectedTerm === 1 ? 'selected' : 'notselected'}
+          className={selectedTerm === 1 ? 'selected' : 'notselected'}
           onClick={() => SelectTerm(1)}
         >
           1M
         </StyledTermButton>
         <StyledBar>|</StyledBar>
         <StyledTermButton
-          className={SelectedTerm === 3 ? 'selected' : 'notselected'}
+          className={selectedTerm === 3 ? 'selected' : 'notselected'}
           onClick={() => SelectTerm(3)}
         >
           3M
         </StyledTermButton>
         <StyledBar>|</StyledBar>
         <StyledTermButton
-          className={SelectedTerm === 6 ? 'selected' : 'notselected'}
+          className={selectedTerm === 6 ? 'selected' : 'notselected'}
           onClick={() => SelectTerm(6)}
         >
           6M
         </StyledTermButton>
-
-        </StyledTop>
-        <StyledBottom></StyledBottom>
-      </StyledCompareProfit>
-      
+      </StyledTop>
+      <StyledBottom>
+        <AccountLineChart />
+      </StyledBottom>
+    </StyledCompareProfit>
   );
 }
 
 const StyledCompareProfit = styled.div`
-  height: 100%  
+  height: 100%;
 `;
 
 const StyledTop = styled.div`
@@ -74,7 +75,7 @@ const StyledBottom = styled.div`
   height: 50vh;
   border: solid 1px #b8a88e;
   border-radius: 10px;
-  background-color: black;
+  background-color: #202020;
   position: relative;
-  margin: 10px
+  margin: 10px;
 `;
