01f297dfe85b5fe4685e3cb4b23ed92fd0e79358
mouse0429 <mouse0429@naver.com>
2022-03-15 22:01:29 +0900

    버블 차트 1차 완성
    버블 차트 1차 완성

--- src/pages/Accountpage/presentational/BubbleChart.jsx
+++ src/pages/Accountpage/presentational/BubbleChart.jsx
@@ -3,64 +3,69 @@ import { BubbleChart, BubbleLabel, BubbleSeries, Bubble, TooltipArea } from 'rea
 
 export default function CustomBubbleChart(props) {
   return (
-    <BottomStyle>
-      <BubbleChart
-        data={[
-          { key: '할리스', data: 40 },
-          { key: '스타벅스', data: 30 },
-          { key: '베르데', data: 17.5 },
-          { key: '이디야', data: 12.5 },
-        ]}
-        height={props.height}
-        width={props.width}
-        center={true}
-        series={
-          <BubbleSeries
-            animated={true}
-            colorScheme={(data) => (data.data > 20 ? '#FFB0BB' : '#FFBFA5')}
-            bubble={
-              <Bubble
-                onClick={(data) => {
-                  console.log(data);
-                }}
-                animated={true}
-                tooltip={<TooltipArea disabled={true} />}
-              />
-            }
-            label={
-              <BubbleLabel
-                fontSize={20}
-                fill="#000"
-                format={(data) => {
-                  return (
-                    <g>
-                      <text
-                        dy={-(data.data.data / 2)}
-                        textAnchor="middle"
-                        fontSize={data.data.data}
-                        fontWeight="bold"
-                      >
-                        {data.data.data + '%'}
-                      </text>
-                      <text
-                        dy={data.data.data}
-                        textAnchor="middle"
-                        fontSize={data.data.data}
-                        fontWeight="bold"
-                      >
-                        {data.data.key}
-                      </text>
-                    </g>
-                  );
-                }}
-              />
-            }
-          />
-        }
-      />
-      <div>총 자산 중</div>
-      <div>10,000,000원</div>
-    </BottomStyle>
+    <div>
+      <AINameStyle>투자 AI 이름</AINameStyle>
+      <StartDateStyle>21.01.04</StartDateStyle>
+      <PlustDayStyle>+00일</PlustDayStyle>
+      <BottomStyle>
+        <BubbleChart
+					style={tempStyle}
+          data={[
+            { key: '할리스', data: 40 },
+            { key: '스타벅스', data: 30 },
+            { key: '베르데', data: 17.5 },
+            { key: '이디야', data: 12.5 },
+          ]}
+          height={props.height*0.8}
+          width={props.width}
+					margin={50}
+          center={true}
+          series={
+            <BubbleSeries
+              animated={true}
+              colorScheme={(data) => (data.data > 20 ? '#FFB0BB' : '#FFBFA5')}
+              bubble={
+                <Bubble
+                  onClick={(data) => {
+                    console.log(data);
+                  }}
+                  animated={true}
+                  tooltip={<TooltipArea disabled={true} />}
+                />
+              }
+              label={
+                <BubbleLabel
+                  format={(data) => {
+                    return (
+                      <g>
+                        <text
+                          dy={-(data.data.data / 4)}
+                          textAnchor="middle"
+                          fontSize={data.data.data/2}
+                          fontWeight="bold"
+                        >
+                          {data.data.data + '%'}
+                        </text>
+                        <text
+                          dy={data.data.data /2}
+                          textAnchor="middle"
+                          fontSize={data.data.data/2}
+                          fontWeight="bold"
+                        >
+                          {data.data.key}
+                        </text>
+                      </g>
+                    );
+                  }}
+                />
+              }
+            />
+          }
+        />
+        <MoneyIntroStyle>총 자산 중</MoneyIntroStyle>
+        <MoneyStyle>10,000,000원</MoneyStyle>
+      </BottomStyle>
+    </div>
   );
 }
 
@@ -69,6 +74,41 @@ display: flex;
 flex-direction: column;
 justify-content: center;
 align-items: center;
-
-
 `;
+
+const AINameStyle = styled.span`
+	position: relative;
+	top: 10px;
+	left: 10px;
+	font-weight: lighter;
+	font-size: 20px;
+`
+
+const StartDateStyle = styled.span`
+	position: relative;
+	top: 10px;
+	left: 100px;
+	font-weight: lighter;
+	font-size: 20px;
+`
+
+const PlustDayStyle = styled.div`
+	position: relative;
+	left: 250px;
+	font-weight: lighter;
+	font-size: 20px;
+`
+
+const MoneyIntroStyle = styled.div`
+	font-size: 18px;
+	font-weight: lighter;
+` 
+
+const MoneyStyle = styled.div`
+	font-weight: bold; 
+	font-size: 42px;
+`
+
+const tempStyle = {
+	margin: "60px"
+}
