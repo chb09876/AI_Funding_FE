f26e95086b92979d7ec65dd28b61c38bb8c5eb15
mouse0429 <mouse0429@naver.com>
2022-03-15 22:39:52 +0900

    스크롤 이슈 해결
    가로로도 스크롤 되는 이슈 해결

--- src/pages/Accountpage/presentational/CheckProfit.jsx
+++ src/pages/Accountpage/presentational/CheckProfit.jsx
@@ -12,55 +12,58 @@ SwiperCore.use([EffectCoverflow, Pagination]);
 
 export default function CheckProfit() {
   const [SelectUnit, SelectPercent] = useState(0);
-	console.log(window.innerHeight, window.innerWidth);
+  console.log(window.innerHeight, window.innerWidth);
   return (
     <StyledLogContainer>
-    <StyledScrollArea>
-    <StyledCheck>
-      <StyledTop className="Top">
-        <StyledSwiper
-          grabCursor={true}
-          centeredSlides={true}
-          slidesPerView={'auto'}
-          pagination={{
-            clickable: true,
-            type: 'bullets',
-            bulletActiveClass: 'swiper-pagination-bullet-active',
-            bulletClass: 'swiper-pagination-bullet-custom swiper-pagination-bullet',
-          }}
-          className="mySwiper"
-        >
-          <SwiperSlide>
-            <div className="Account1">
-              <Account1 />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="Account2">
-              <Account2 />
-            </div>
-          </SwiperSlide>
-          <SwiperSlide>
-            <div className="Account3">
-             <Account3/>
-            </div>
+      <StyledScrollArea>
+        <StyledCheck>
+          <StyledTop className="Top">
+            <StyledSwiper
+              grabCursor={true}
+              centeredSlides={true}
+              slidesPerView={'auto'}
+              pagination={{
+                clickable: true,
+                type: 'bullets',
+                bulletActiveClass: 'swiper-pagination-bullet-active',
+                bulletClass: 'swiper-pagination-bullet-custom swiper-pagination-bullet',
+              }}
+              className="mySwiper"
+            >
+              <SwiperSlide>
+                <div className="Account1">
+                  <Account1 />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="Account2">
+                  <Account2 />
+                </div>
+              </SwiperSlide>
+              <SwiperSlide>
+                <div className="Account3">
+                  <Account3 />
+                </div>
+              </SwiperSlide>
+            </StyledSwiper>
+          </StyledTop>
 
-          </SwiperSlide>
-        </StyledSwiper>
-      </StyledTop>
-
-			<StyledBottom>
-					<CustomBubbleChart height={window.innerHeight*35/100} width={window.innerWidth-20}/>
-        </StyledBottom>
-
-      </StyledCheck>
+          <StyledBottom>
+						
+            <CustomBubbleChart
+              height={(window.innerHeight * 35) / 100}
+              width={window.innerWidth -20}
+            />
+          </StyledBottom>
+        </StyledCheck>
       </StyledScrollArea>
     </StyledLogContainer>
   );
-};
+}
 
 const StyledScrollArea = styled.div`
-  overflow: scroll;
+  overflow-y: scroll;
+	overflow-x: hidden;
   height: 95%;
 `;
 
@@ -69,13 +72,12 @@ const StyledLogContainer = styled.div`
 `;
 
 const StyledTop = styled.div`
-  flex-grow : 1;
+  flex-grow: 1;
   width: 95%;
   border: solid 1px #b8a88e;
   border-radius: 10px;
   background-color: black;
   margin: 10px auto;
-  
 `;
 
 const StyledSwiper = styled(Swiper)`
@@ -87,10 +89,10 @@ const StyledSwiper = styled(Swiper)`
 const StyledCheck = styled.div``;
 
 const StyledBottom = styled.div`
-height: 45vh;
-width: 95%;
-border: solid 1px #b8a88e;
-border-radius: 10px;
-background-color: black;
-margin: 10px auto;
+  height: 45vh;
+  width: 95%;
+  border: solid 1px #b8a88e;
+  border-radius: 10px;
+  background-color: black;
+  margin: 10px auto;
 `;
