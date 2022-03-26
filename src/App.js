35ce936a4e198adf6ccab0f488516ec6d9975e9e
chb09876 <chb09876@naver.com>
2022-03-26 15:40:06 +0900

    feat: 자동 로그인 구현

--- src/App.js
+++ src/App.js
@@ -1,16 +1,67 @@
+import { useEffect } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import BackgroundLayout from './common/BackgroundLayout';
 import MenuNav from './common/MenuNav';
 import Home from './pages/home/presentational/Home';
 import Login from './pages/login/container/Login';
 import KakaoAuth from './pages/login/container/KakaoAuth';
-import GoogleAuth from './pages/login/container/GoogleAuth'
-import { useSelector } from 'react-redux';
+import GoogleAuth from './pages/login/container/GoogleAuth';
+import { useDispatch, useSelector } from 'react-redux';
 import AiPages from './pages/AI-pages/presentation/AiPages';
 import AccountPage from './pages/Accountpage/presentational/AccountPage';
+import { getItem } from './utils/cookies';
+import axios from 'axios';
+import { signIn, updateToken } from './modules/login';
 
 export default function App() {
   const isLoggedIn = useSelector((state) => state.login.isLoggedIn);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    try {
+      // automatically login logic
+      if (isLoggedIn === false) {
+        const hasRefresh = getItem('has_refresh');
+        if (hasRefresh === 'true') {
+          // request new access token with posting refresh token(cookie)
+          axios
+            .get(`${process.env.REACT_APP_API_TEST}/auth/token`, { withCredentials: true })
+            .then((response) => {
+              const { accessToken, UID } = response.data;
+              dispatch(signIn(accessToken, UID));
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch, isLoggedIn]);
+
+  useEffect(() => {
+    try {
+      // request access token every 25 minutes
+      const interval = setInterval(() => {
+        axios
+          .post(`${process.env.REACT_APP_API_HOST}/auth/token`)
+          .then((response) => {
+            const { accessToken } = response.data;
+            dispatch(updateToken(accessToken));
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }, 25 * 60 * 1000);
+
+      return () => {
+        clearInterval(interval);
+      };
+    } catch (error) {
+      console.log(error);
+    }
+  }, [dispatch]);
 
   return (
     <BackgroundLayout className="App">
@@ -20,7 +71,7 @@ export default function App() {
           <>
             <Route path="/" element={<Home />} />
             <Route path="/second" element={<AiPages />} />
-            <Route path="/third" element={<AccountPage/>} />
+            <Route path="/third" element={<AccountPage />} />
             <Route path="/fourth" element={'Community page'} />
             <Route path="/fifth" element={'Settings page'} />
             <Route path="*" element={'404'} />
