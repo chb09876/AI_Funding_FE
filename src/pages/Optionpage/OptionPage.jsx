c06bea978941a87e9040d4bfe56d7b94dd2143bf
BaekSeungGeon <rjsdkdlsdk@naver.com>
2022-07-16 20:26:46 +0900

    수정
    기초 수정

--- src/pages/Optionpage/OptionPage.jsx
+++ src/pages/Optionpage/OptionPage.jsx
@@ -176,110 +176,6 @@ const TopButten = styled.div`
 const StyledCommunityPage = styled.div`
   height: 100%;
 `;
-const StyledTabs = styled.div`
-  display: flex;
-  width: 100%;
-  height: 60px;
-  background: rgb(32, 32, 32);
-`;
-
-const StyledTab = styled.div`
-  margin-top: 10px;
-  width: 100%;
-`;
-
-const DivisionTab = styled.div`
-line-height: 45px;
-color: rgb(119, 119, 119);
-font-size: 14px;
-`;
-
-//우측정렬, 좌측정렬 위한 스타일 태그
-const DisplayFlex = styled.div`
-    display:flex;
-`;
-
-const StyledTitle = styled.div`
-  width: 100%;
-  text-align: center;
-  height: 100%;
-  line-height: 45px;
-  font-size: 18px;
-  color: rgb(152, 128, 101);
-  border-bottom: 2px solid rgb(152, 128, 101);
-  background : yellow;
-`;
-
-const StyledTabButton = styled.div`
-  line-height: 45px;
-  font-size: 14px;
-  margin-left:14px;
-  margin-right:14px;
-  ${(props) => {
-    if (props.className === 'selected') {
-      return `color: rgb(152, 128, 101);
-      `;
-    } else if (props.className === 'notselected') {
-      return `color: rgb(119, 119, 119);
-      
-      `;
-    } else {
-      return `color: rgb(119, 119, 119);
-      `;
-    }
-  }};
-`;
-
-//인기게시글 레이아웃
-const HotBoardTab = styled.div`
-  width:96%;
-  height:80px;
-  margin-left:2%;
-  padding:10px;
-  border-right: 1px solid rgb(152, 128, 101);
-  border-left: 1px solid rgb(152, 128, 101);
-  border-top: 1px solid rgb(152, 128, 101);
-  border-radius:5px 5px 5px 5px;
-  border-bottom: 1px solid rgb(152, 128, 101);
-`;
-
-//HOT
-const HotTitile = styled.div`
-  color:#ED6A60;
-  margin-right:5px;
-  font-size:17px;
-`;
-
-//인기 게시글 제목
-const HotTitleContent = styled.div`
-  color:white;
-  font-size:17px;
-`;
-
-//인기게시글, 게시글내용 좋아요,댓글 레이아웃
-const BottomTab = styled.div`
-margin-top:20px;
-display:flex;
-width:100%;
-font-size:15px;
-`;
-
-//날짜
-const BoardDate = styled.div`
-  color: rgb(119, 119, 119);
-`;
-
-//좋아요
-const Like = styled.div`
-  color:#ED6A60;
-  margin-left:auto;  
-`; //margin-left:auto; 로 우측정렬
-
-//말풍선
-const Comment = styled.div`
-  color:#61C454;
-  margin-left:5px;
-`;
 
 //게시글, 댓글 레이아웃
 const OptionTab = styled.div`
@@ -295,14 +191,6 @@ const OptionTab = styled.div`
 const OptionName = styled.div`
   color:white;
   font-size:22px;
-`;
-
-//게시글 좋아요,댓글 레이아웃
-const BoardBottomTab = styled.div`
-margin-top:13px;
-display:flex;
-width:100%;
-font-size:15px;
 `;
 
 //스크롤
