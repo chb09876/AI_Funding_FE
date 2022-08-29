9a4cec99f6ecc4fe76620f751e683999b5dda68c
chb09876 <chb09876@naver.com>
2022-08-29 18:54:15 +0900

    feat: 앱 버전 및 업데이트 날짜 dotenv 환경변수에 추가

--- scripts/makeEnv.sh
+++ scripts/makeEnv.sh
@@ -1,6 +1,7 @@
 # !/bin/bash
-if [ $# -eq 1 ]; then
-    echo "REACT_APP_API=$1" > .env
+if [ $# -eq 2 ]; then
+    DATE=$(date "+%F %H:%M")
+    echo -e "REACT_APP_API=$1\nREACT_APP_VERSION=$2\nREACT_APP_UPDATE_DATE=\"$DATE\"" > .env
 else
     echo "$0 arguments is invalid."
 fi
