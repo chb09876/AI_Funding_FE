245e0578f416a1a7d1c74d9c12947299acca9fcd
angrymusic <58168512+angrymusic@users.noreply.github.com>
2022-09-06 13:34:52 +0900

    AI 소개 페이지 반응형 이슈 해결

--- src/utils/validator.js
+++ src/utils/validator.js
@@ -1,20 +1,20 @@
-import Joi from 'joi';
+// import Joi from 'joi';
 
-export const emailValidator = (email) => {
-  return !(
-    'error' in
-    Joi.string()
-      .email({ tlds: { allow: false } })
-      .validate(email)
-  );
-};
+// export const emailValidator = (email) => {
+//   return !(
+//     'error' in
+//     Joi.string()
+//       .email({ tlds: { allow: false } })
+//       .validate(email)
+//   );
+// };
 
-export const nicknameValidator = (nickname) => {
-  return !(
-    'error' in
-    Joi.string()
-      .pattern(/^[a-zA-Z0-9가-힣]*$/)
-      .max(12)
-      .validate(nickname)
-  );
-};
+// export const nicknameValidator = (nickname) => {
+//   return !(
+//     'error' in
+//     Joi.string()
+//       .pattern(/^[a-zA-Z0-9가-힣]*$/)
+//       .max(12)
+//       .validate(nickname)
+//   );
+// };
