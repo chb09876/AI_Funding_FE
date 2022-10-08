a4a5cd153f42337ddf55c837eb97ffb18db22c65
chb09876 <chb09876@naver.com>
2022-10-08 16:36:32 +0900

    feat: 로딩 페이지 컴포넌트 제작

---
+++ src/common/Loading.jsx
@@ -0,0 +1,58 @@
+import styled from 'styled-components';
+
+export default function Loading() {
+  return (
+    <LoadingContainer>
+      <Spinner>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </Spinner>
+    </LoadingContainer>
+  );
+}
+
+const LoadingContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Spinner = styled.div`
+  display: inline-block;
+  position: relative;
+  width: 80px;
+  height: 80px;
+
+  & div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid rgb(184, 168, 142);
+    border-radius: 50%;
+    animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: rgb(184, 168, 142) transparent transparent transparent;
+  }
+  & div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  & div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  & div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes lds-ring {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
+  }
+`;
